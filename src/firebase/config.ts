86d86e0c71aa2b0601d32dad678d0caffc536804
Vailan D'Souza <vailan5002@gmail.com>
2026-02-10 15:02:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-890917444-8ed95",
+  "appId": "1:881499834162:web:e5d2e80c48003d0d61b92b",
+  "apiKey": "AIzaSyDHG7xPcGUwLSwlkxpYSl0POpmJ4vSepE4",
+  "authDomain": "studio-890917444-8ed95.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "881499834162"
+};
